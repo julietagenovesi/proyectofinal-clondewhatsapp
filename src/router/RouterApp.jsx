3f78ae6f95b4073b8028f2d0da3251b0d3612eea
julietagenovesi <genovesijulieta1@gmail.com>
2025-10-11 20:55:12 -0300

    Contexto aplicado a chat

--- src/router/RouterApp.jsx
+++ src/router/RouterApp.jsx
@@ -3,18 +3,22 @@ import { Login } from "../views/Login";
 import { Messages } from "../views/Messages";
 import { NotFound } from "../views/NotFound";
 import ProtectedRoute from "../components/ProtectedRoute";
+import { ModeComponent } from "../context/ModeContext";
 
 const RouterApp = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/chat"
-          element={
-            <ProtectedRoute>
-              <Messages />
-            </ProtectedRoute>}
-        />
+        <ModeComponent>
+          <Route path="/chat"
+            element={
+              <ProtectedRoute>
+                <Messages />
+              </ProtectedRoute>}
+          />
+        </ModeComponent>
+
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
